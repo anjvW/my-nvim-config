3ca392a67270d04b7a2884b7ae9b18076f3775b2
anjv <anjv@qq.com>
2025-04-06 22:18:49 +0800

    fix bug

--- init.lua
+++ init.lua
@@ -3,18 +3,27 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- 基础设置
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.termguicolors = true
+local opt = vim.opt
+
+-- 设置基本选项
+opt.number = true
+opt.relativenumber = true
+opt.mouse = 'a'
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.wrap = true
+opt.breakindent = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.termguicolors = true
+opt.fileencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.splitbelow = true
+
+-- 设置 shell
+opt.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'bash'
 
 -- 透明背景设置
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
@@ -23,15 +32,14 @@ vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
 
 -- 终端设置
-vim.opt.splitbelow = true              -- 在底部打开新窗口
-vim.opt.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'bash'  -- 优先使用 PowerShell
 vim.api.nvim_create_autocmd('TermOpen', {
   pattern = '*',
   callback = function()
-    vim.opt_local.number = false          -- 终端窗口不显示行号
-    vim.opt_local.relativenumber = false  -- 终端窗口不显示相对行号
-    vim.opt_local.signcolumn = 'no'       -- 终端窗口不显示标记列
-    vim.cmd('startinsert')                -- 自动进入插入模式
+    local term_opt = vim.opt_local
+    term_opt.number = false
+    term_opt.relativenumber = false
+    term_opt.signcolumn = 'no'
+    vim.cmd('startinsert')
   end,
 })
 
@@ -55,18 +63,14 @@ require("lazy").setup("plugins")
 -- 加载核心配置
 require("core.keymaps")
 
--- 设置文件编码为 UTF-8
-vim.opt.fileencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-
 -- Windows 系统下的编码设置
 if vim.fn.has('win32') == 1 then
-    vim.opt.fileencoding = "utf-8"
-    vim.opt.fileencodings = "utf-8,ucs-bom,cp936,gb18030,big5,euc-jp,euc-kr,latin1"
+    opt.fileencoding = "utf-8"
+    opt.fileencodings = "utf-8,ucs-bom,cp936,gb18030,big5,euc-jp,euc-kr,latin1"
 end
 
 -- OpenAI 配置
-vim.env.OPENAI_API_KEY = vim.fn.getenv('deepseek_key')  -- 从环境变量获取 API 密钥
+vim.env.OPENAI_API_KEY = vim.fn.getenv('deepseek_key')
 vim.env.OPENAI_API_HOST = "https://api.deepseek.com"
 
 -- 如果需要代理
